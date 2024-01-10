6019cfddc43e35fc847d883acd9a042796f0f542
Salud3 <78719641+Salud3@users.noreply.github.com>
2024-01-10 09:51:37 -0600

    innitialcommit

---
+++ Apuntadores/Apuntadores.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, * dir_num;
+    num = 20;
+    dir_num = &num;
+
+    cout << "Numero: " << num << endl;//muestra el numero
+    cout << "Direccion de memoria del numero: " << &num << endl;//muestra la posicion de memoria
+
+    cout << "Numero: " << *dir_num << endl;//muestra el numero
+    cout << "Direccion de memoria del numero: " << dir_num << endl;//muestra la posicion de memoria
+    //La direccion y  puntero deben ser el mismo tipo de variable
+    //------------------------
+
+    int awa, * dir_awa;
+
+    cout << "Introduce un numero: " << endl;
+
+    cin >> awa;
+    dir_awa = &awa;
+    if (awa % 2 == 0)
+    {
+        cout << "El numero: " << *dir_awa << " es par" << endl;
+        cout << "Guardado en: " << dir_awa << endl;
+    }
+    else
+    {
+        cout << "El numero: " << *dir_awa << " es impar" << endl;
+        cout << "Guardado en: " << dir_awa << endl;
+    }
+
+
+
+
+}
